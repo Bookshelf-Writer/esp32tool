954e7171040af16cea989619fbf0cd185556e957
SunSung W541 <mail@embedded.biz.ua>
2024-08-05 00:40:58 +0200

    main [v0.0.1]  zero ver

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package esp32tool
 const (
 	GlobalName       string = "esp32tool"
 	GlobalDateUpdate string = "08-05-2024"
-	GlobalHash       string = "32f8526ff70cf96ccc212808be5f078af537d770"
+	GlobalHash       string = "cfad9cfb3f84ecc4a9c70651a1cd4572ac28416a"
 
-	GlobalVersion      string = "v0.0.4"
+	GlobalVersion      string = "v0.0.1"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 4
+	GlobalVersionPatch uint16 = 1
 )
